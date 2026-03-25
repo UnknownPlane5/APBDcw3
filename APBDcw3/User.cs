9b56f876453d274e4af40ed3f84b16abf47fdc68
UnknownPlane5 <78647561+UnknownPlane5@users.noreply.github.com>
2026-03-25 13:08:01 +0100

    Commit do gałęzi  AbstactionImplementation. Implementacja działanie podklas Device, pozbycie się SetDescription,dodanie enum CameraType i refaktoryzacja szczegółów.

--- APBDcw3/User.cs
+++ APBDcw3/User.cs
@@ -2,8 +2,8 @@ namespace APBDcw3;
 
 public abstract class User
 {
-    protected int id { get; set; }
-    protected string name{ get; set; }
-    protected string surname { get; set; }
-    protected int maxRentalLimit { get; }
+    protected int Id { get; set; }
+    protected string Name{ get; set; }
+    protected string Surname { get; set; }
+    protected int MaxRentalLimit { get; }
 }
