1215e5db8024102cc54f55f444a81098d945b2cd
UnknownPlane5 <78647561+UnknownPlane5@users.noreply.github.com>
2026-03-25 13:20:39 +0100

    Commit do gałęzi AbstractImplementation. Zmodyfikowano user i zaimplementowano Employee i Student

--- APBDcw3/User.cs
+++ APBDcw3/User.cs
@@ -2,8 +2,14 @@ namespace APBDcw3;
 
 public abstract class User
 {
-    protected int Id { get; set; }
+    protected static int Id { get; set; }
     protected string Name{ get; set; }
     protected string Surname { get; set; }
-    protected int MaxRentalLimit { get; }
+    public abstract int MaxRentalLimit { get; }
+    protected User (int id, string name, string surname)
+    {
+        Id = id;
+        Name = name;
+        Surname = surname;
+    }
 }
